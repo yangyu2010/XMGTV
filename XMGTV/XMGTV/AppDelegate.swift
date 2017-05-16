5c3a7d892713d60f2cef676d2397344a7c1157e4
YangYu <yangyu2010@aliyun.com>
2017-05-16 16:43:17 +0800

    init

---
+++ XMGTV/XMGTV/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  XMGTV
+//
+//  Created by apple on 16/11/8.
+//  Copyright © 2016年 coderwhy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
